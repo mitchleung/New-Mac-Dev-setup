bb01a178263dc47a9ab623f74d9d09bf6ebf2222
Mitch Leung <mitch@tekcent.com>
2023-03-08 12:07:47 +1300

    Add more dev tools and macos utilities

--- new-mac.sh
+++ new-mac.sh
@@ -14,8 +14,10 @@ brew tap mongodb/brew;brew update;brew install mongodb-community@6.0;
 #openvpn-connect for remote work #wfh
 #cloudflare-warp for better and safer DNS
 #macs-fan-control shows the fan speed
+#satellite-eyes shows the map around your location in various styles
 #brooklyn a stylish screen saver
 #monitorcontrol if you are using monitor not supported for brightness control
+#menuwhere access meun anywhere
 #whalebird for #mastodon
 #gimp for bitmap graphic manipulation
 #inkscape for vector graphic manipulation
@@ -24,25 +26,38 @@ brew tap mongodb/brew;brew update;brew install mongodb-community@6.0;
 #firefox & google-chrome for browser testing
 #cyberduck for sFPT
 #diffmerge to diff between files
+#docker for easier deployment
+#miniconda for python lib
 #owasp-zap for web app scanner
 #discord for all servers / communities you are in
 #tableplus for access to mongodb in GUI
+#mongodb-compass if you need gui to access mongodb
 #cinebench & geekbench for #benchmark your new mac
 #grandperspective to see what's chunking up your file system
 #sloth shows all open files and sockets in use
 #macfuse in case u need to mount volumes not supported by #macos
 #xquartz if you need X11, e.g. #xeyes 👀
 
-brew install --cask --no-quarantine barrier numi rectangle openvpn-connect cloudflare-warp macs-fan-control brooklyn monitorcontrol whalebird gimp inkscape handbrake vlc firefox google-chrome cyberduck diffmerge owasp-zap discord tableplus cinebench geekbench grandperspective sloth macfuse xquartz;
+brew install --cask --no-quarantine barrier numi rectangle openvpn-connect macs-fan-control satellite-eyes brooklyn monitorcontrol menuwhere whalebird gimp inkscape handbrake vlc firefox google-chrome cyberduck diffmerge docker owasp-zap discord tableplus mongodb-compass cinebench geekbench grandperspective sloth xquartz;
 
 #install command line tools
 #nvm for multiple node version for projects, also setup its path
 #btop shows network, cpu usage and more except GPU I believe over Apple's Activity Monitor
-brew install nvm btop;
+#neofetch shows system information
+#speedtest-cli - command line to speedtest.net
+
+brew install nvm btop neofetch speedtest-cli;
+
+#optional oh-my-zsh and p10k
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+brew install romkatv/powerlevel10k/powerlevel10k
+echo "source $(brew --prefix)/opt/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
 
 #configure nvm path
 mkdir ~/.nvm;
-echo "export NVM_DIR="$HOME/.nvm" \ [ -s "/usr/local/opt/nvm/nvm.sh" ] && \. "/usr/local/opt/nvm/nvm.sh"  # This loads nvm \[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion" >> ~/.zshrc
+echo "export NVM_DIR="$HOME/.nvm" \ 
+[ -s "/usr/local/opt/nvm/nvm.sh" ] && \. "/usr/local/opt/nvm/nvm.sh"  # This loads nvm \
+[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion" >> ~/.zshrc
 
 #start mongdb as service
 brew services start mongodb/brew/mongodb-community
