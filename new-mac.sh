50d0535d87da316d20dc7056a947fa0caf4f4d3e
Mitch Leung <mitchleung.com@gmail.com>
2025-03-06 11:20:23 +1300

    2025 tools update

--- new-mac.sh
+++ new-mac.sh
@@ -8,59 +8,64 @@ brew update; brew upgrade;
 brew tap mongodb/brew;brew update;brew install mongodb-community@6.0;
 
 #install mac applications
-#barrier for sharing #mouse & #keyboard across different #computers on different OS
-#numi for quick and easy calculation, currency conversion and etc
-#rectangle a handy window manager
-#openvpn-connect for remote work #wfh
-#cloudflare-warp for better and safer DNS
-#macs-fan-control shows the fan speed
-#satellite-eyes shows the map around your location in various styles
-#brooklyn a stylish screen saver
-#monitorcontrol if you are using monitor not supported for brightness control
-#menuwhere access meun anywhere
-#whalebird for #mastodon
-#gimp for bitmap graphic manipulation
-#inkscape for vector graphic manipulation
-#handbrake for video compression
-#vlc for viewing any video formats
-#firefox & google-chrome for browser testing
-#cyberduck for sFPT
-#diffmerge to diff between files
-#docker for easier deployment
-#miniconda for python lib
-#owasp-zap for web app scanner
-#discord for all servers / communities you are in
-#tableplus for access to mongodb in GUI
-#mongodb-compass if you need gui to access mongodb
-#cinebench & geekbench for #benchmark your new mac
-#grandperspective to see what's chunking up your file system
-#sloth shows all open files and sockets in use
-#macfuse in case u need to mount volumes not supported by #macos
-#xquartz if you need X11, e.g. #xeyes 👀
-#itsycal shows tiny calendar at menu bar
-#signal-messenger - secure messenger
-#imageoptim - compress images
-#postman - for REST API testing
-#appzapper - for app management
-#aldente - for battery management
-#miniconda - for python lib
-#sonic-pi - for music programming
-#duet - for remote display
+# - **aerial** - macOS screensaver that can use videos of your own as well as community videos around the world.  
+# - **aldente** - battery management tool that helps control charging limits to extend battery lifespan.  
+# - **anytype** - privacy-focused, offline-first, all-in-one knowledge management and note-taking app.  
+# - **appzapper** - utility that completely removes applications and their related files from macOS.  
+# - **arc** - modern, privacy-focused web browser by The Browser Company.  
+# - **audacity** - free, open-source audio editor and recorder.  
+# - **calibre** - powerful eBook manager and converter.  
+# - **cyberduck** - file transfer tool supporting FTP, SFTP, WebDAV, and cloud storage like AWS S3.  
+# - **devtoys** - collection of developer tools, like JSON formatting, regex testing, and more.  
+# - **diffmerge** - visual comparison and merging tool for files and folders.  
+# - **discord** - communication platform for voice, video, and text, popular among gamers and communities.  
+# - **docker** - platform for developing, shipping, and running containerized applications.  
+# - **duet** – Turns an iPad into a secondary display for a Mac or PC.  
+# - **firefox** - fast, privacy-focused web browser from Mozilla.  
+# - **freetube** - privacy-focused YouTube client that allows ad-free and tracker-free viewing.  
+# - **gimp** - free and open-source image editor, often considered an alternative to Photoshop.  
+# - **google-chrome** – Google’s web browser.  
+# - **grandperspective** - disk space visualization tool that helps identify large files and folders.  
+# - **handbrake** - free video transcoder for converting media files into different formats.  
+# - **imageoptim** - tool for optimizing and compressing images without losing quality.  
+# - **inkscape** - free and open-source vector graphics editor, similar to Adobe Illustrator.  
+# - **itsycal** - lightweight calendar app that integrates with the macOS menu bar.  
+# - **jordanbaird-ice** - macOS menu bar utility that helps hide and organize excessive menu bar icons for a cleaner look.
+# - **krita** - open-source digital painting and illustration tool.  
+# - **macs-fan-control** - utility to monitor and manually adjust Mac’s fan speeds.  
+# - **miniconda** - minimal installation of Conda for managing Python environments and packages.  
+# - **mongodb-compass** - GUI tool for managing MongoDB databases.  
+# - **monitorcontrol** - macOS utility to control external display brightness and volume using keyboard shortcuts.  
+# - **mx-power-gadget** - monitoring tool that displays CPU power usage and temperature on Macs.  
+# - **numi** - simple, handy calculator and converter for macOS from menu bar. 
+# - **obs** – popular tool for screen recording and live streaming.  
+# - **openemu** - multi-system video game emulator for macOS.  
+# - **openvpn-connect** - VPN client for connecting to OpenVPN servers.  
+# - **oracle-jdk** – Oracle’s Java Development Kit for developing Java applications.  
+# - **pika** - color picker tool for macOS with accessibility features.  
+# - **postman** – API development and testing tool for sending HTTP requests.  
+# - **prepros** - preprocessor tool for compiling and optimizing web development assets like Sass and TypeScript.  
+# - **rectangle** - macOS window management tool for snapping and organizing windows.  
+# - **slack** - team collaboration and messaging app.  
+# - **sloth** - macOS process viewer that helps visualize open files, sockets, and system resources.  
+# - **upscayl** – AI-powered image upscaler for enhancing low-resolution images.  
+# - **vlc** - versatile, open-source media player that supports almost all video and audio formats.  
+# - **zed** - fast, collaborative code editor designed for performance.
 
-brew install --cask --no-quarantine barrier numi rectangle openvpn-connect macs-fan-control satellite-eyes brooklyn monitorcontrol menuwhere whalebird gimp inkscape handbrake vlc firefox google-chrome cyberduck diffmerge docker owasp-zap discord tableplus mongodb-compass cinebench geekbench grandperspective sloth itsycal signal-messenger imageoptim postman appzapper aldente miniconda sonic-pi duet;
+brew install --cask --no-quarantine  aerial aldente anytype appzapper arc audacity calibre cyberduck devtoys diffmerge discord docker duet firefox freetube gimp google-chrome grandperspective handbrake imageoptim inkscape itsycal jordanbaird-ice krita macs-fan-control miniconda mongodb-compass monitorcontrol mx-power-gadget numi obs openemu openvpn-connect oracle-jdk pika postman prepros rectangle slack sloth upscayl vlc zed 
 
 #install command line tools
 #nvm for multiple node version for projects, also setup its path
-#btop shows network, cpu usage and more except GPU I believe over Apple's Activity Monitor
-#neofetch shows system information
-#speedtest-cli - command line to speedtest.net
+#btop shows network, cpu usage
+#htop shows network, cpu usage
+#speedtest - command line to speedtest.net
 
-brew install nvm btop neofetch speedtest-cli cmake protobuf rust python@3.10 wget ffmpeg influxdb;
+brew install nvm btop htop speedtest cmake rust python@3.10 wget ffmpeg;
 
 #optional oh-my-zsh and p10k
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-brew install romkatv/powerlevel10k/powerlevel10k
-echo "source $(brew --prefix)/opt/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# brew install romkatv/powerlevel10k/powerlevel10k
+# echo "source $(brew --prefix)/opt/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
 
 #configure nvm path
 mkdir ~/.nvm;
